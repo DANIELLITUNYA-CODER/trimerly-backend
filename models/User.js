b3496efae5efa6d3e84db28aa23fdc9c6352aacb
DANIEL LITUNYA <144170836+DANIELLITUNYA-CODER@users.noreply.github.com>
2024-07-16 16:43:43 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,18 @@
-// models/User.js
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  fullName: { type: String },
-  phoneNumber: { type: String },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
+const User = mongoose.model('User', userSchema);
+module.exports = User;
